456494033e8178d10747744ae754c35470170016
Marcos Solinas <marcsebasolinas@gmail.com>
2024-07-04 18:52:10 -0300

    creando desafio de clients

---
+++ src/main/java/com/abmcoder/abm/repositories/ClientsRepository.java
@@ -0,0 +1,10 @@
+package com.abmcoder.abm.repositories;
+
+import com.abmcoder.abm.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientsRepository extends JpaRepository<Client, Long> {
+
+}
